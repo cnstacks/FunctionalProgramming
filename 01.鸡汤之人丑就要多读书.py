ba598f53b5eb4116bc2fee6794a4696c37072e5e
tqtl911@163.com <19930911cXS>
2018-09-19 23:16:21 +0800

    Initial commit

---
+++ 01.鸡汤之人丑就要多读书.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: FunctionalProgramming 
+# Software: PyCharm
+# Time    : 2018-09-19 23:15
+# File    : 01.鸡汤之人丑就要多读书.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+
+
+
+
