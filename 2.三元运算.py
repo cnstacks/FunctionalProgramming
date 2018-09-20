9e687afea439c01994eca55787b1d63546517d81
tqtl911@163.com <19930911cXS>
2018-09-20 12:00:48 +0800

    三元运算。

---
+++ 2.三元运算.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: FunctionalProgramming 
+# Software: PyCharm
+# Time    : 2018-09-20 12:00
+# File    : 2.三元运算.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+
+
