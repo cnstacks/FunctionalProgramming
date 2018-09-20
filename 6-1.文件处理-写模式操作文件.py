cdf37ea6d330ed6dcaac5d2b99293529b342ab22
tqtl911@163.com <19930911cXS>
2018-09-20 12:45:19 +0800

    6-1文件处理-写模式操作文件

---
+++ 6-1.文件处理-写模式操作文件.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: FunctionalProgramming 
+# Software: PyCharm
+# Time    : 2018-09-20 12:31
+# File    : 6-1文件处理-写模式操作文件.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+
+""
+""" 
+1、边读边处理方式处理文件；
+2、默认有一个换行符\n，循环时候自动添加一个空行，so 为2个；
+3、w模式永远是清空操作，如果文件存在，先清空再写入，一定要慎用！
+"""
+# f = open("兼职白领学生空姐模特护士联系方式1.txt", "wb")
+# f.write("崔晓昭".encode("gbk"))
+# f.close()
+
+
+f = open("兼职白领学生空姐模特护士联系方式1.txt", "wb")
+f.write("cuixiaozhao".encode("gbk"))
+f.close()
---
+++ 6.文件处理-写模式操作文件.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: FunctionalProgramming 
+# Software: PyCharm
+# Time    : 2018-09-20 12:31
+# File    : 6.文件处理-写模式操作文件.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+
+""
+""" 
+1、边读边处理方式处理文件；
+2、默认有一个换行符\n，循环时候自动添加一个空行，so 为2个；
+"""
+
+# f = open("兼职白领学生空姐模特护士联系方式.txt.bak", "r", encoding="gbk")
+# for line in f:
+#     print("按照行循环打印:", line)
+# f.close()
+
+
+f = open()
